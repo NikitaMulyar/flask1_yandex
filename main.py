c0ae1feceec6a96afee06638eec9ea8f67a2d2ff
NikitaMulyar <nickm.school@mail.ru>
2023-02-02 09:43:59 +0300

    Задача Реклама с картинкой

--- main.py
+++ main.py
@@ -31,10 +31,38 @@ def return_sample_page():
                   <body>
                     <h1>Жди нас, Марс!</h1>
                   </body>
-                  <img src="/static/images/mars.jpg" height="216" width="288" alt="здесь должна была быть картинка, но не нашлась"/>
+                  <img src="/static/img/mars.jpg" height="216" width="288" alt="здесь должна была быть картинка, но не нашлась"/>
                   <body>
                     <p>Вот она какая, красная планета.</p>
                   </body>
+                </html>"""
+
+
+@app.route('/promotion_image')
+def promo_image():
+    text = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+            'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
+            'Присоединяйся!']
+    colors = ['dark', 'success', 'secondary', 'warning', 'danger']
+    html_text = "".join([f"""<div class="alert alert-{colors[i]}" role="alert">
+  {text[i]}
+</div>"""for i in range(5)])
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                  </body>
+                  <img src="/static/img/mars.jpg" height="216" width="288" alt="здесь должна была быть картинка, но не нашлась"/>
+                  <br/><br/>
+                  {html_text}
                 </html>"""
 
 
